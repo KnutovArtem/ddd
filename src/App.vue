bdead8c68ea494eb72080df2b5c4ddaf32c48fcc
artem_knutov <artyomknutov@emailsoldiers.ru>
2021-12-04 15:34:36 +0300

    Saving the development process.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,22 @@
 <template>
+  <div class='info' hidden>
+    <p>Шаг: <span>{{ this.step }}</span></p>
+    <p>Категоря: <span>{{
+        this.category_id === 0 ? "Хочу машину" : this.category_id === 1 ? "Куплю квартиру" : this.category_id === 2 ? "Закрыть все кредиты" : this.category_id === 3 ? "Открою своё дело" : this.category_id === 4 ? "Просто деньги" : null
+                       }} [{{ this.category_id }}]
+      </span>
+    </p>
+    <p>Сумма: <span :class='{blue : this.amount === 3}'>{{
+        this.amount === 0 ? "до 500" : this.amount === 1 ? "до 1 млн" : this.amount === 2 ? "до 3 млн" : this.amount === 3 ? "более 3 млн" : null
+                                                        }} [{{ this.amount }}]
+      </span>
+    </p>
+    <p>Способ оплаты: <span>{{
+        this.purchase_method === 0 ? "Кредит" : this.purchase_method === 1 ? "Лизинг" : this.purchase_method === 2 ? "Откладывать от з.п." : null
+                            }} [{{ this.purchase_method }}]
+      </span>
+    </p>
+  </div>
   <main class='main'>
 
     <Header/>
@@ -103,6 +121,7 @@
 
         <div class='section-text'>
           <p class='section-title' v-html='this.your_choice_title'/>
+
           <div class='section-desc'>
             <p v-for='desc in this.your_choice_desc' :key='desc' v-html='desc'/>
           </div>
@@ -126,8 +145,7 @@
           <div class='section-buttons'>
             <button class='button--answer'
                     v-for='(button_answer, index) in this.test_buttons' :key='index'
-                    :class='{true: this.button_active === button_answer["answer"] && button_answer["answer"] === 0,
-                               false: this.button_active === button_answer["answer"] && button_answer["answer"] === 1}'
+                    :class='activeClassButton(button_answer)'
                     @click="getAnswer(button_answer['answer']);"
                     v-html='button_answer["name"]'/>
           </div>
@@ -156,8 +174,15 @@
           <div class='table-credits'>
             <p class='table'></p>
             <p class='section-title'> Ну что, убедили? </p>
-            <button class='button--black' @click='this.step = 7; this.result_type_credit = 0; getData(this.data);'> Хочу автокредит</button>
-            <button class='button--white' @click='this.step = 7; this.result_type_credit = 1; getData(this.data);'> Хочу потребительский</button>
+            <div class='buttons' v-if='this.amount !== 3'> <!--todo: удалить лишнее-->
+              <button class='button--black' @click='finalStep(0)'> Хочу автокредит</button>
+              <button class='button--white' @click='finalStep(1)'> Хочу потребительский</button>
+            </div>
+            <div class='buttons' v-if='this.amount === 3'>
+              <button class='button--white' @click='finalStep(3)'> Хочу кредит под залог недвижимости </button>
+              <button class='button--white' @click='finalStep(1)'> Хочу потребительский</button>
+              <button class='button--bd' @click='this.step=8'>Всё ещё сомневаюсь</button>
+            </div>
           </div>
           <button class='button--back' @click='this.show_button = false; stepPrev();'> Назад</button>
 
@@ -224,7 +249,7 @@
             <button class='button--black' @click='this.step = 7; this.result_type_credit = 2; getData(this.data);'>6,9% — пушка</button>
             <button class='button--bd' @click='this.common = false'>Хм, залог мне не подходит</button>
           </div>
-          <button class='button--back' @click='!this.table_credits ? this.table_credits = true : stepPrev();'> Назад</button>
+          <button class='button--back' @click='stepPrev();'> Назад</button>
         </div>
 
         <div class='section-text' v-if='!this.common'>
@@ -233,11 +258,11 @@
           </p>
           <p>Ну что, готов промчаться с ветерком на новом авто? </p>
           <div class='buttons'>
-            <button class='button--bd' @click='this.step = 7; this.result_type_credit = 0; getData(this.data);'>Оформить автокредит</button>
-            <button class='button--bd' @click='this.step = 7; this.result_type_credit = 1; getData(this.data);'>Оформить потребительский кредит</button>
+            <button class='button--bd' @click='this.finalStep(0)'>Оформить автокредит</button>
+            <button class='button--bd' @click='this.finalStep(1)'>Оформить потребительский кредит</button>
             <button class='button--bd' @click='this.step=8'>Всё ещё сомневаюсь</button>
           </div>
-          <button class='button--back' @click='!this.table_credits ? this.table_credits = true : stepPrev();'> Назад</button>
+          <button class='button--back' @click='stepPrev();'> Назад</button>
         </div>
 
       </div>
@@ -256,14 +281,14 @@
           <p>Если нужна сумма побольше, выбирай кредит под залог недвижимости. А если у тебя нет своей, можно договориться с родственниками, чтобы разрешили оформить кредит под залог их квартиры. Квартира останется у них в собственности, а тебе могут одобрить сумму до 15 млн со ставкой от 6,9%. При этом никакой волокиты: для оформления потребуются только паспорт и СНИЛС. </p>
           <p>Оба варианта хороши. Выбирай тот, который больше подходит, и вперёд — к квартире своей мечты!</p>
           <div class='buttons'>
-            <button class='button--bd' @click='this.step = 7; this.result_type_credit = 1; getData(this.data);'>Оформить потребительский кредит</button>
-            <button class='button--bd' @click='this.step = 7; this.result_type_credit = 2; getData(this.data);'>Оформить кредит с залогом</button>
+            <button class='button--bd' @click='this.finalStep(1)'>Оформить потребительский кредит</button>
+            <button class='button--bd' @click='this.finalStep(2)'>Оформить кредит с залогом</button>
+            <button class='button--back' @click='stepPrev();'> Назад</button>
           </div>
         </div>
 
 
       </div>
-
 
       <!--Finish... -->
       <div class='section' v-if='this.step === 10'>
@@ -279,6 +304,7 @@
       </div>
 
     </div>
+
   </main>
 </template>
 
@@ -295,16 +321,13 @@ export default {
   data() {
     return {
       data: [],
+      start: true,
+      next: false,
+      step: 0,
       title: "",
       your_choice_title: "",
       your_choice_desc: "",
       your_choice_button: "",
-      test_question: "",
-      test_buttons: "",
-
-      /*---------*/
-      start: true,
-      step: 0,
       category_id: 0,
       amount: 0,
       purchase_method: 0,
@@ -314,57 +337,86 @@ export default {
       current_question: 0,
       all_qestions: 7,
       attempt: 0,
+      test_question: "",
+      test_version: "",
+      test_buttons: "",
       correct_answer: "",
       correct_answer_text: "",
       answer_text: false,
       show_button: false,
-      table_credits: true,
       result: [],
       result_type_credit: 0,
 
       // info lizing
       leasing: [],
       text_info: 0,
+      test_desc: "",
       common: true,
 
-      //
+      // other
       button_active: false,
-      next: false,
 
     }
   },
 
   methods: {
+    finalStep(type_credit) {
+      this.step               = 7;
+      this.result_type_credit = type_credit;
+      this.getData(this.data);
+    },
+    activeClassButton(button_answer) {
+      return {
+        true: this.button_active === button_answer["answer"] && button_answer["answer"] === 0,
+        false: this.button_active === button_answer["answer"] && button_answer["answer"] === 1
+      }
+    },
     getData(data) {
-      console.clear();
-      console.log('1');
-      this.all_qestions = data['categories'][this.purchase_method]['test'];
+
+      if(this.amount === 3) {
+        this.test_version = 'test_2';
+        this.purchase_method !== 2 ? this.test_desc = 'desc_2' : this.test_desc = 'desc';
+      }else {
+        this.test_version = 'test';
+        this.test_desc = 'desc';
+      }
+
+      this.all_qestions = data['categories'][this.purchase_method][this.test_version];
       this.result       = data["result"][this.result_type_credit];
       this.title        = data['titles'][this.step];
 
       /*твой выбор*/
-      console.log('2');
       this.your_choice_title  = data['categories'][this.purchase_method]['title'];
-      this.your_choice_desc   = data['categories'][this.purchase_method]['desc'];
+      this.your_choice_desc   = data['categories'][this.purchase_method][this.test_desc];
       this.your_choice_button = data['categories'][this.purchase_method]['name_button'];
 
       /*тест*/
-      //if (this.step === 2 && this.purchase_method === 0) {
       if (this.purchase_method === 0) {
-        console.log('3');
-        this.test_question = data['categories'][this.purchase_method]['test'][this.current_question]["question"];
-        console.log('33');
-        this.current_question >= 0 ? this.test_buttons = data['categories'][this.purchase_method]['test'][this.current_question]['buttons'] : null;
+        this.test_question = data['categories'][this.purchase_method][this.test_version][this.current_question]["question"];
+        this.current_question >= 0 ? this.test_buttons = data['categories'][this.purchase_method][this.test_version][this.current_question]['buttons'] : null;
       }
-      console.log('333');
-      this.step === 4 && this.next ? this.correct_answer_text = data["categories"][this.purchase_method]["test"][this.current_question]["answers"][this.correct_answer]["text"] : null;
+      this.step === 4 && this.next ? this.correct_answer_text = data["categories"][this.purchase_method][this.test_version][this.current_question]["answers"][this.correct_answer]["text"] : null;
 
       /*Инфо. лизинг*/
-      console.log('4');
       this.leasing = data['categories'][this.purchase_method]['info'];
     },
     stepPrev: function () {
-      this.step === 6 ? this.step = 4 : this.step === 7 ? this.step = 5 : this.step--;
+
+      switch (this.step) {
+        case 6:
+          this.step = 4;
+          break;
+        case 7:
+          this.amount === 3 ? this.step = 4 : this.step = 5;
+          break;
+        case 8:
+          this.step = 6;
+          break;
+        default:
+          this.step--;
+          break;
+      }
+      //this.step === 6 ? this.step = 4 : this.step === 7 ? this.step = 5 : this.step === 8 ? this.step = 6 : this.step--;
       this.buttons       = this.data["variants_buttons"][this.step];
       this.button_active = false;
       this.next          = false;
@@ -399,14 +451,26 @@ export default {
         that.attempt = 1;
       }
 
-      if (this.current_question !== 6) {
+      if (this.current_question !== this.all_qestions.length - 1) {
         this.attempt < 1 ? resetAnswer(this) : null;
       } else {
         this.show_button = true;
         this.attempt < 1 ? button_answer === 1 ? resetAnswer(this) : this.step++ : null;
       }
 
+      // More that one million
+      let stopTest = this.amount === 3 && this.current_question + 1 === this.all_qestions.length;
+      // if button = true
+      if (stopTest && button_answer === 0) {
+        this.finalStep(2);
+      }
+      // if button = false // todo: removed
+      /*if (stopTest && button_answer === 1) {
+      }*/
+
     },
+
+    // Следующий вопрос.
     qestionNext: function () {
       if (this.next) {
         this.current_question++;
@@ -417,12 +481,12 @@ export default {
       this.attempt       = 0;
       this.getData(this.data);
     },
+
+    // Предыдующий вопрос.
     qestionPrev: function () {
-      if (this.current_question === 0) {
-        this.step--;
-      }
-      this.buttons = this.data["variants_buttons"][this.step]
+      this.current_question === 0 ? this.step-- : null;
       this.current_question !== 0 ? this.current_question-- : null;
+      this.buttons       = this.data["variants_buttons"][this.step]
       this.next          = false;
       this.answer_text   = false;
       this.button_active = false;
