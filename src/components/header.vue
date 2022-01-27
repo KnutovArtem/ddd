bd88174c6d3bbe1de84f84fa52800fc155684fe6
arvkn <arv.knutov@gmail.com>
2022-01-27 09:55:12 +0300

    Small changes in the text.  Social network icons are hidden

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,8 +1,7 @@
 <template>
   <header class='header'>
-
     <a class='header__logo' href='https://www.tinkoff.ru/' target='_blank'></a>
-    <div class='header__social'>
+    <div class='header__social' v-if='this.step === 10'>
       <a class='icon icon-ins' href='https://www.instagram.com/TinkoffBank/' target='_blank'></a>
       <a class='icon icon-youtube' href='https://www.youtube.com/user/tcsbank' target='_blank'></a>
       <a class='icon icon-vk' href='https://vk.com/tinkoffbank' target='_blank'></a>
@@ -15,6 +14,9 @@
 <script>
 export default {
   name: 'FirstScreen',
+  props: {
+    step: {type: String}
+  },
 }
 </script>
 
